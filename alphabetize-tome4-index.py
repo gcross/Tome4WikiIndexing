443f7ee50844008ebb7a8abd2b9a918ff4b493a3
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-02-28 18:44:04 -0800

    Removed the alphabet index as internal links are not supported.

--- alphabetize-tome4-index.py
+++ alphabetize-tome4-index.py
@@ -16,25 +16,16 @@ prelude = []
 index = None
 letters = set()
 
-alphabet_sentinel = "<!-- Alphabet goes here -->"
-index_sentinel = "<!-- Index goes here -->"
-skip_next_line = False
-
 import fileinput
 for line in fileinput.input(files=(input_filename,),openhook=lambda input_filename,mode: open(input_filename,mode) if input_filename is not None else sys.stdin):
-    if skip_next_line:
-        skip_next_line = False
-        continue
     line = line.strip()
-    if line == alphabet_sentinel:
-        skip_next_line = True
     if index is None:
-        if line == index_sentinel:
+        if line[:1] == "=":
             index = {}
         else:
             prelude.append(line)
     if index is not None:
-        if not line or line[0] != ':':
+        if line[:1] != ':':
             continue
         try:
             value, key = line[3:-2].split('|')
@@ -51,13 +42,8 @@ for line in fileinput.input(files=(input_filename,),openhook=lambda input_filena
 
 with open(output_filename,mode='w') if output_filename is not None else sys.stdout as f:
     for line in prelude:
-        if line == alphabet_sentinel:
-            print(alphabet_sentinel,file=f)
-            print(' '.join('[[#{0:}|{0:}]]'.format(letter) for letter in sorted(letters)),file=f)
-        else:
-            print(line,file=f)
+        print(line,file=f)
 
-    print(index_sentinel,file=f)
     last_letter = None
     for key, value in sorted(index.items()):
         if key[0] != last_letter:
