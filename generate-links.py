865dfde726a52a48d4b810f7a8e084c77360f139
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-02-28 23:18:47 -0800

    Wrote a script to generate links for a list of topics.

---
+++ generate-links.py
@@ -0,0 +1,13 @@
+# This script is placed in the public domain.  It is hosted at https://github.com/gcross/Tome4WikiIndexing.
+
+# Needed to make the script work with both Python 2.7 and 3.2
+from __future__ import print_function
+
+import sys
+link = sys.argv[1]
+kind = sys.argv[2]
+for talent in sys.stdin.read().split('\n'):
+    talent = talent.strip()
+    if not talent:
+        continue
+    print("[[{}|{} ({})]]".format(link,talent,kind))
